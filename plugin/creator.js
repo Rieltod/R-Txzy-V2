287e63a311b75320894075f7afa1b3d788554faa
Rieltod <99353680+Rieltod@users.noreply.github.com>
2022-02-20 18:30:06 +0800

    Update creator.js

--- plugin/creator.js
+++ plugin/creator.js
@@ -16,7 +16,7 @@ async function handler(m) {
   let njid3 = number3 + '@s.whatsapp.net'
   let onW3 = await this.isOnWhatsApp(njid3) || { isBusiness: false }
 
-let name4 = 'RielXzy'
+let name4 = 'RielXzy🥀'
   number4 = owner[3].replace(/[^0-9]/g, '')
   let njid4 = number4 + '@s.whatsapp.net'
   let onW4 = await this.isOnWhatsApp(njid4) || { isBusiness: false }
